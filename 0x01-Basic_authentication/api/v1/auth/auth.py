bb0748d1e55d2c495daf65da6ddc8ffa0c3edb11
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-20 16:35:36 +0000

    Defining basic Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,10 +8,13 @@ from typing import List, TypeVar
 class Auth:
     '''API Authentication class manager'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''require auth method'''
         return False
     
     def authorization_header(self, request=None) -> str:
+        '''authorization_header method'''
         return None
     
     def current_user(self, request=None) -> TypeVar('User'):
+        '''current_user method'''
         return None
