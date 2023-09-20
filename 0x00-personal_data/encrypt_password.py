1d34d3f6bc1d8e077a80f5142e9343e3704f8c9d
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-20 13:07:59 +0000

    Checking if a hased password is identical to a passowrd

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     '''Encrypts a password'''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Checks if the hashed password and the password are identical'''
+    return bcrypt.checkpw(password.encode(), hashed_password)
