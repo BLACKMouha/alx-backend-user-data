d7a1ee80a8237957781bce9ab92781e9415541f1
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-20 12:01:17 +0000

    get_db returns a connection to a database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -56,7 +56,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     db = os.getenv('PERSONAL_DATA_DB_NAME')
 
-    return mysql.connector.connect(
+    return mysql.connector.MySQLConnection(
         host=host,
         user=user,
         password=password,
