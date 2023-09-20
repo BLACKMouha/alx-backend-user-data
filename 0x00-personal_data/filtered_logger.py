2bf97a1a95a513ec27d234ad1d674944dfa61120
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-20 08:51:04 +0000

    RedactingFormatter.format uses old-style string formatting to fill placholder and computes a log line with obfuscated fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -23,3 +23,23 @@ def filter_datum(fields: List[str],
             f'{field}={redaction}',
             filter_log_line)
     return re.sub(';', separator, filter_log_line)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(fmt=self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''Fitlers values in incoming log records'''
+        from datetime import datetime
+        s = self.FORMAT % {'name': record.name, 'levelname': record.levelname,
+                           'asctime': datetime.now(), 'message': record.msg}
+        return filter_datum(self.fields, self.REDACTION, s, self.SEPARATOR)
