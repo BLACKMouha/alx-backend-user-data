e76141f2f30c85046f47c1beae4f42f2eb7bcff7
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-20 21:28:56 +0000

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,10 @@ from typing import List, TypeVar
 class Auth:
     '''API Authentication class manager'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        '''returns True if the path is not in the excluded paths list'''
+        '''A path requires authorization if it is not in the list of excluded
+        paths. All paths that are in the excluded paths don't need
+        authorization to be accessed.
+        '''
         if not path or not excluded_paths:
             return True
         if not path.endswith('/'):
@@ -17,7 +20,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         '''authorization_header method'''
-        return None
+        if not request:
+            return None
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''current_user method'''
