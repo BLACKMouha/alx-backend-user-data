5b58f76751acad4b28cfc97a9b1ea87ba1818b98
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-22 11:07:14 +0000

    Unfortunately the regex does not work . So I implemnt again in another way to make more flexible Auth.require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,21 +7,19 @@ from typing import List, TypeVar
 
 class Auth:
     '''API Authentication class manager'''
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''A path requires authorization if it is not in the list of excluded
         paths. All paths that are in the excluded paths don't need
         authorization to be accessed.
         '''
-        import re
 
         if not path or not excluded_paths:
             return True
         for excluded_path in excluded_paths:
-            regex = re.compile('{}'.format(
-                excluded_path.replace('/', '\\/').replace('*', '.*')))
-            regex.search(path)
-            match = regex.search(string=path)
-            if match:
+            if excluded_path.endswith('*') and \
+                    path.startswith(excluded_path[:-1]) or \
+                    excluded_path in [path, path + '/']:
                 return False
         return True
 
