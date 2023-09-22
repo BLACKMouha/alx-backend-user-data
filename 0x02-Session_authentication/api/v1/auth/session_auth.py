2675d560d2dc3090ff00e2744d94ec08468a838f
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-22 11:53:37 +0000

    Adding a class attribute user_id_by_session_id (a dictionary which key is the session_id because it should be unique, and the value is a user ID so that a User can have multiple session) which holds all users session. Session.create_session creates a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,18 @@
 '''session_auth module'''
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     '''New authentication mechanism for simulating Users Sessions'''
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''Creates a new Session ID for a user_id'''
+        if not isinstance(user_id, str):
+            return None
+        session_id = uuid.uuid4().__str__()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
