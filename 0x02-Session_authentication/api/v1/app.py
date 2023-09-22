228e51499f1aa8e6d90e1d18ff7e6da108e6f50b
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-22 13:21:38 +0000

    app.befor_request adds '/api/v1/auth_session/login/'  path in the excluded_paths that require authorization and also raise a 401

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,10 +34,13 @@ def before_request():
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/']
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/']
     print(request.headers)
     if not auth.require_auth(request.path, excluded_paths):
         return
+    if auth.session_cookie(request):
+        return None, abort(401)
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
