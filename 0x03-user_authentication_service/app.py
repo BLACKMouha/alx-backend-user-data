3b596ae5e1b5eace3bc82bb1482d1005ade94706
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-24 09:35:57 +0000

    User Registration Flask endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 '''Flask app'''
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from sqlalchemy.orm.exc import NoResultFound
+from auth import Auth, _hash_password
+
+AUTH = Auth()
 
 app = Flask(__name__)
+
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -12,6 +17,25 @@ def root() -> dict:
     Returns {'message': 'bienvenue'}
     """
     return jsonify({"message": "Bienvenue"})
+
+d = {'a': 1, 'b': 2}
+d.__contains__('a')
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    '''User registeration endpoint'''
+    if all(request.form.__contains__(k) for k in request.form):
+        email = request.form.get('email', None)
+        password = request.form.get('password', None)
+        print(email, password)
+        try:
+            user = AUTH.register_user(
+                email=email, password=password)
+            return jsonify({'email': email, 'message': 'user created'})
+        except ValueError:
+            return jsonify({'message': 'email already registered'})
+    else:
+        return jsonify({'message': 'missing email or password'})
 
 
 if __name__ == "__main__":
