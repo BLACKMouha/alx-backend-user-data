40536c1f00983b8c0a5a164db119b2970140dd37
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-24 10:17:45 +0000

    Auth.valid_login if a given email and password match a User

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -37,3 +37,13 @@ class Auth:
             self._db._session.add(new_user)
             self._db._session.commit()
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        '''Checking if the arguments matches a user'''
+        try:
+            user = self._db.find_user_by(email=email)
+            hashed_password = user.hashed_password
+            return bcrypt.checkpw(password.encode(), hashed_password)
+        except NoResultFound:
+            print('exception raised: No result found!')
+            return False
