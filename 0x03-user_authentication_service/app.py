4fc7916b83d285ff67d0f4732b67d4c3919874ff
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-24 10:02:16 +0000

    User Registration Flask endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,7 +10,6 @@ AUTH = Auth()
 app = Flask(__name__)
 
 
-
 @app.route('/', methods=['GET'], strict_slashes=False)
 def root() -> dict:
     """ GET /
@@ -18,8 +17,6 @@ def root() -> dict:
     """
     return jsonify({"message": "Bienvenue"})
 
-d = {'a': 1, 'b': 2}
-d.__contains__('a')
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
@@ -32,6 +29,7 @@ def users():
         return jsonify({'email': email, 'message': 'user created'}), 200
     except ValueError:
         return jsonify({'message': 'email already registered'}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
