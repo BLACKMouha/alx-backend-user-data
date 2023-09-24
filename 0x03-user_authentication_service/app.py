bc536bc3226f49459290f72f50f736e5281ef953
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-24 12:33:03 +0000

    Given an email, the user recieves a reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,5 +65,20 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    '''Gets the reset password token'''
+    email = request.form.get('email', None)
+    if not email:
+        abort(403)
+    try:
+        reset_token = AUTH.get_reset_password_token(email=email)
+    except ValueError:
+        reset_token = None
+    if not reset_token:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
